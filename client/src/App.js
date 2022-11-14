30a117a585974ba49cb7f697de8c57505dd25f5d
Mziaf <ziaf.nadeem@retailo.co>
2022-11-14 18:29:42 +0500

    Remove unused variables.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React from "react";
-import * as lodash from "lodash";
 
 const projectAURL = "http://localhost:3000";
 const projectBURL = "http://localhost:3001";
